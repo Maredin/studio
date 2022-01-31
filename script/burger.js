23e6df3c4b1d7ae83541cbd27ef0ad8c3a6cec22
Marat <mringer@bk.ru>
2022-01-31 14:10:48 +0500

    burger

---
+++ script/burger.js
@@ -0,0 +1,30 @@
+//----BURGER ----
+
+var show_menu = document.getElementById('show-menu');
+var hidden_menu = document.getElementById('hidden-menu');
+
+var burger1 = document.getElementById('burger1');
+var burger2 = document.getElementById('burger2');
+var burger3 = document.getElementById('burger3');
+
+let hide = document.querySelectorAll('#hidden-menu ul li');
+
+
+show_menu.addEventListener('click', function(e){
+   hidden_menu.style.right = hidden_menu.style.right === '0px' ? '-75%' : '0px';
+    burger2.style.display = burger2.style.display === 'none' ? 'block' : 'none';
+    burger1.style.transform = burger1.style.transform === 'translateY(10px) rotate(-45deg)' ? 'translateY(0px) rotate(0deg)' : 'translateY(10px) rotate(-45deg)';
+    burger3.style.transform = burger3.style.transform === 'translateY(3px) rotate(45deg)' ? 'translateY(0px) rotate(0deg)' : 'translateY(3px) rotate(45deg)';
+});
+
+
+hide.forEach(item => {
+    item.addEventListener('click', () => {
+        hidden_menu.style.right = hidden_menu.style.right === '0px' ? '-75%' : '0px';
+    burger2.style.display = burger2.style.display === 'none' ? 'block' : 'none';
+    burger1.style.transform = burger1.style.transform === 'translateY(10px) rotate(-45deg)' ? 'translateY(0px) rotate(0deg)' : 'translateY(10px) rotate(-45deg)';
+    burger3.style.transform = burger3.style.transform === 'translateY(3px) rotate(45deg)' ? 'translateY(0px) rotate(0deg)' : 'translateY(3px) rotate(45deg)';
+    });
+});
+
+console.log(hide);
--- script/modal.js
+++ script/modal.js
@@ -1,10 +1,18 @@
 const btnModal = document.querySelector('.row4_info button'),
       popap = document.querySelector('.popup');
 
-
+function setModal() {
+    popap.style.display = 'none';
+    document.body.style.overflow = '';
+}
 btnModal.addEventListener('click', () => {
     popap.style.display = 'block';
-    setTimeout(() => {
+    document.body.style.overflow = 'hidden';
+    setTimeout(setModal, 3000);
+});
+popap.addEventListener('click', (e) => {
+    if(e.target == popap) {
         popap.style.display = 'none';
-    }, 3000);
+        document.body.style.overflow = '';
+    }
 });
